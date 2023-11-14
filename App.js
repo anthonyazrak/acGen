2d085f4cc5cbbb42ee481b9341bee5c6b49165bb
anthonyazrak <anthony.azrak1@gmail.com>
2023-11-14 13:19:22 -0500

    updates

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import HomeScreen from "./pages/HomePage"
+import SavedScreen from './pages/SavedPage';
+import GenerateScreen from './pages/GeneratePage';
+import AccountScreen from './pages/AccountPage';
+import PictureScreen from './pages/PicturePage';
+import { Feather } from '@expo/vector-icons'; 
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" options={
+          {
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Feather name="home" size={24} color="black" />
+            ),
+            
+          }
+        
+        } component={HomeScreen} />
+        <Tab.Screen name="Savec Activities" options={
+          {
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Feather name="bookmark" size={24} color="black" />
+            ),
+          }
+        
+        
+        } component={SavedScreen} />
+        <Tab.Screen name="Generate" options={
+          {
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+                 <MaterialIcons name="bolt" size={48} color="black" />
+            ),
+          }
+        
+        } component={GenerateScreen} />
+        <Tab.Screen name="Picture" options={
+          {
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="images-outline" size={24} color="black" />
+            ),
+          }
+
+        } component={PictureScreen} />
+        <Tab.Screen name="Account" options={
+          {
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="user-circle" size={24} color="black" />
+            ),
+          }
+
+        } component={AccountScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pages/GeneratePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function GenerateScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "white" }}>
+        <Text>Generate Screen</Text>
+      </View>
+    );
+  }
+
+export default GenerateScreen;
