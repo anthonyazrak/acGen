899d7a1862a1806884247437233d29e682fca4da
Martin Kruchinski <martin.kruchinskialmeida@mail.mcgill.ca>
2023-11-15 17:17:09 -0500

    Pictures home page

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, Button, Image, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, FlatList, TouchableOpacity, Platform, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 function HomeScreen() {
@@ -9,27 +9,44 @@ function HomeScreen() {
     // ... other activities
   ]);
 
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          Alert.alert('Permission Required', 'Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
   const pickImage = async (id) => {
-    // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
-
+  
+    console.log(result); // Log the result to see what data is returned
+  
     if (!result.cancelled) {
-      setActivities(activities.map(activity => {
+      // Extract the uri from the first item in the assets array
+      const uri = result.assets[0].uri;
+      console.log('Updating activity with ID:', id, 'with URI:', uri); // Log the URI being set
+  
+      setActivities(currentActivities => currentActivities.map(activity => {
         if (activity.id === id) {
-          return { ...activity, imageUri: result.uri };
+          return { ...activity, imageUri: uri };
         }
         return activity;
       }));
     }
   };
+  
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "white" }}>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
       <FlatList
         data={activities}
         keyExtractor={(item) => item.id}
@@ -38,11 +55,11 @@ function HomeScreen() {
             {item.imageUri ? (
               <Image source={{ uri: item.imageUri }} style={{ width: 200, height: 200 }} />
             ) : (
-              <TouchableOpacity onPress={() => pickImage(item.id)}>
+              <TouchableOpacity onPress={() => pickImage(item.id)} style={{ alignItems: 'center', justifyContent: 'center', height: 200, width: 200, backgroundColor: '#ddd' }}>
                 <Text>Add a picture</Text>
               </TouchableOpacity>
             )}
-            <Text>{item.name}</Text>
+            <Text style={{ marginTop: 10 }}>{item.name}</Text>
           </View>
         )}
       />
