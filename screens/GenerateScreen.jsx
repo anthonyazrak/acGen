25972949205be119a4e14b87a471bf948ec94e01
theSaab <saba.fathi@emailgmail.info>
2023-11-15 17:44:07 -0500

    added backend for accuonts

--- screens/GenerateScreen.jsx
+++ screens/GenerateScreen.jsx
@@ -1,252 +1,224 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
-import Slider from '@react-native-community/slider';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import Slider from "@react-native-community/slider";
 
 function GenerateScreen() {
-    const [price, setPrice] = useState(10);
-    const [numberOfPeople, setNumberOfPeople] = useState(1);
-    const [selectedLocation, setSelectedLocation] = useState('');
-    const [timeOfDay, setTimeOfDay] = useState(12); // New state for time of the day
+  const API_KEY = "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";
 
-    const locations = ['Home', 'School', 'Forest', 'Beach', 'Park', 'Lake'];
+  const [price, setPrice] = useState(10);
+  const [numberOfPeople, setNumberOfPeople] = useState(1);
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [timeOfDay, setTimeOfDay] = useState(12); // New state for time of the day
+  const [loadingActivity, setLoadingActivity] = useState(false);
+  const [response, setResponse] = useState("");
+  const locations = ["Home", "School", "Forest", "Beach", "Park", "Lake"];
 
-    const selectLocation = (location) => {
-        setSelectedLocation(location);
-    };
+  const selectLocation = (location) => {
+    setSelectedLocation(location);
+  };
 
-    const handleGeneratePress = () => {
-        console.log('Generate button pressed');
-    };
+  const handleGeneratePress = () => {
+    console.log("Generate button pressed");
+  };
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <ScrollView style={styles.scrollView}>
-                <Text style={styles.header}>Choose a location</Text>
-                <View style={styles.locationContainer}>
-                    {locations.map((location) => (
-                        <TouchableOpacity
-                            key={location}
-                            style={[
-                                styles.locationButton,
-                                selectedLocation === location && styles.selectedLocation,
-                            ]}
-                            onPress={() => selectLocation(location)}
-                        >
-                            <Text style={styles.locationText}>{location}</Text>
-                        </TouchableOpacity>
-                    ))}
-                </View>
+  const API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
 
-                <View style={styles.sliderContainer}>
-                    <Text style={styles.label}>Budget: ${price}</Text>
-                    <Slider
-                        style={styles.slider}
-                        value={price}
-                        onValueChange={setPrice}
-                        minimumValue={0}
-                        maximumValue={100}
-                        step={10}
-                    />
-                </View>
+  const sendPromptToChatGPT = async (prompt) => {
+    try {
+      const response = await fetch(API_ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content:
+                "You are an activity recommendation engine, you must generate an activity that is tailored to my situation in this current moment.",
+            },
+            {
+              role: "user",
+              content: prompt,
+            },
+          ],
+        }),
+      });
 
-                <View style={styles.sliderContainer}>
-                    <Text style={styles.label}>Number of People: {numberOfPeople}</Text>
-                    <Slider
-                        style={styles.slider}
-                        value={numberOfPeople}
-                        onValueChange={setNumberOfPeople}
-                        minimumValue={1}
-                        maximumValue={10}
-                        step={1}
-                    />
-                </View>
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
 
-                <View style={styles.sliderContainer}>
-                    <Text style={styles.label}>Time of the Day: {timeOfDay}h</Text>
-                    <Slider
-                        style={styles.slider}
-                        value={timeOfDay}
-                        onValueChange={setTimeOfDay}
-                        minimumValue={0}
-                        maximumValue={24}
-                        step={1}
-                    />
-                </View>
+      const result = await response.json();
+      const chatGPTResponse = result.choices[0].message.content;
 
-                <TouchableOpacity 
-                    style={styles.generateButton} 
-                    onPress={handleGeneratePress}
-                >
-                    <Text style={styles.generateButtonText}>Generate</Text>
-                </TouchableOpacity>
-            </ScrollView>
-        </SafeAreaView>
-    );
+      return chatGPTResponse;
+    } catch (error) {
+      console.error("Error sending prompt to ChatGPT:", error);
+      throw error;
+    }
+  };
+
+  const handleGenerateActivity = async () => {
+    try {
+      setLoadingActivity(true);
+      const prompt = `Suggest an activity for me. My current setting is ${selectedLocation}, my price range for budget is ${price}. and I am in a group of ${numberOfPeople}.`;
+      console.log(prompt);
+      await sendPromptToChatGPT(prompt)
+        .then((response) => {
+          setResponse(response);
+          console.log(response);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } catch (error) {
+      console.error("Error generating activity:", error);
+    } finally {
+      setLoadingActivity(false);
+    }
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.header}>Choose a location</Text>
+        <View style={styles.locationContainer}>
+          {locations.map((location) => (
+            <TouchableOpacity
+              key={location}
+              style={[
+                styles.locationButton,
+                selectedLocation === location && styles.selectedLocation,
+              ]}
+              onPress={() => selectLocation(location)}
+            >
+              <Text style={styles.locationText}>{location}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <View style={styles.sliderContainer}>
+          <Text style={styles.label}>Budget: ${price}</Text>
+          <Slider
+            style={styles.slider}
+            value={price}
+            onValueChange={setPrice}
+            minimumValue={0}
+            maximumValue={100}
+            step={10}
+          />
+        </View>
+
+        <View style={styles.sliderContainer}>
+          <Text style={styles.label}>Number of People: {numberOfPeople}</Text>
+          <Slider
+            style={styles.slider}
+            value={numberOfPeople}
+            onValueChange={setNumberOfPeople}
+            minimumValue={1}
+            maximumValue={10}
+            step={1}
+          />
+        </View>
+
+        <View style={styles.sliderContainer}>
+          <Text style={styles.label}>Time of the Day: {timeOfDay}h</Text>
+          <Slider
+            style={styles.slider}
+            value={timeOfDay}
+            onValueChange={setTimeOfDay}
+            minimumValue={0}
+            maximumValue={24}
+            step={1}
+          />
+        </View>
+
+        <TouchableOpacity
+          style={
+            !loadingActivity ? styles.generateButton : styles.loadingButton
+          }
+          onPress={handleGenerateActivity}
+          disabled={loadingActivity}
+        >
+          <Text style={styles.generateButtonText}>Generate</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    scrollView: {
-        padding: 20,
-        backgroundColor: '#fff',
-    },
-    header: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#333',
-        marginBottom: 20,
-    },
-    locationContainer: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-        marginBottom: 20,
-    },
-    locationButton: {
-        padding: 10,
-        borderWidth: 1,
-        borderColor: '#ddd',
-        borderRadius: 20,
-        marginBottom: 10,
-        alignItems: 'center',
-        backgroundColor: '#f5f5f5',
-        minWidth: '48%', // Ensure at least two buttons fit in one row
-    },
-    selectedLocation: {
-        backgroundColor: '#c2f0c2', // Highlight color for selected location
-    },
-    locationText: {
-        fontSize: 16,
-    },
-    sliderContainer: {
-        marginBottom: 20,
-    },
-    label: {
-        fontSize: 18,
-        marginBottom: 10,
-    },
-    slider: {
-        height: 40,
-    },
-    generateButton: {
-        backgroundColor: '#000', // Black background
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-        marginBottom: 20,
-    },
-    generateButtonText: {
-        color: '#fff', // White text color
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
+  scrollView: {
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 20,
+  },
+  locationContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  locationButton: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 20,
+    marginBottom: 10,
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+    minWidth: "48%", // Ensure at least two buttons fit in one row
+  },
+  selectedLocation: {
+    backgroundColor: "#c2f0c2", // Highlight color for selected location
+  },
+  locationText: {
+    fontSize: 16,
+  },
+  sliderContainer: {
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  slider: {
+    height: 40,
+  },
+  generateButton: {
+    backgroundColor: "#000", // Black background
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  loadingButton: {
+    backgroundColor: "gray", // Black background
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  generateButtonText: {
+    color: "#ffffff", // White text color
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
 
-
 export default GenerateScreen;
-
-
-// import React, { useState } from 'react';
-// import { View, Text, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
-
-// function GenerateScreen() {
-//     const [selectedId, setSelectedId] = useState(null);
-
-//     const options = [
-//         { id: 1, title: 'At home' },
-//         { id: 2, title: 'Beach' },
-//         { id: 3, title: 'Forest' },
-//         { id: 4, title: 'Park' },
-//     ];
-
-//     const handlePress = (id) => {
-//         if (selectedId === id) {
-//             setSelectedId(null); // Deselect if already selected
-//         } else {
-//             setSelectedId(id); // Select the new option
-//         }
-//     };
-
-//     return (
-//         <View style={styles.container}>
-//             <Text style={styles.headerText}>Setting</Text>
-//             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.scrollView}>
-//                 {options.map((item) => (
-//                     <TouchableOpacity
-//                         key={item.id}
-//                         style={[styles.option, selectedId === item.id ? styles.selectedOption : null]}
-//                         onPress={() => handlePress(item.id)}
-//                     >
-//                         <Text style={styles.optionText}>{item.title}</Text>
-//                     </TouchableOpacity>
-//                 ))}
-//             </ScrollView>
-//             <Text style={styles.headerText}>Setting</Text>
-//             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.scrollView}>
-//                 {options.map((item) => (
-//                     <TouchableOpacity
-//                         key={item.id}
-//                         style={[styles.option, selectedId === item.id ? styles.selectedOption : null]}
-//                         onPress={() => handlePress(item.id)}
-//                     >
-//                         <Text style={styles.optionText}>{item.title}</Text>
-//                     </TouchableOpacity>
-//                 ))}
-//             </ScrollView>
-//             <Text style={styles.headerText}>Setting</Text>
-//             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.scrollView}>
-//                 {options.map((item) => (
-//                     <TouchableOpacity
-//                         key={item.id}
-//                         style={[styles.option, selectedId === item.id ? styles.selectedOption : null]}
-//                         onPress={() => handlePress(item.id)}
-//                     >
-//                         <Text style={styles.optionText}>{item.title}</Text>
-//                     </TouchableOpacity>
-//                 ))}
-//             </ScrollView>
-//         </View>
-//     );
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         alignItems: 'center',
-//         padding: 20,
-//         backgroundColor: 'white',
-//     },
-//     headerText: {
-//         fontSize: 24,
-//         fontWeight: 'bold',
-//         marginVertical: 20,
-//     },
-//     scrollView: {
-//         flexDirection: 'row',
-//     },
-//     option: {
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//         backgroundColor: 'white',
-//         padding: 20,
-//         borderRadius: 20,
-//         marginHorizontal: 10,
-//         shadowColor: '#000',
-//         shadowOffset: {
-//             width: 0,
-//             height: 2,
-//         },
-//         shadowOpacity: 0.25,
-//         shadowRadius: 3.84,
-//         elevation: 5,
-//         height: 80
-//     },
-//     selectedOption: {
-//         backgroundColor: 'lightblue', // Change the color for selected option
-//     },
-//     optionText: {
-//         fontSize: 20,
-//         fontWeight: 'bold',
-//     },
-// });
-
-// export default GenerateScreen;
---
+++ services/activity.js
@@ -0,0 +1,83 @@
+import { db } from "./firebase";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  doc,
+  deleteDoc,
+  addDoc,
+  getDoc,
+  updateDoc,
+  orderBy,
+  Timestamp,
+} from "firebase/firestore";
+
+// Fetch all activities created by a specific UID for landing page
+const getActivitiesByUid = async (uid) => {
+  try {
+    const activityRef = collection(db, "activities");
+    const q = query(activityRef, where("userId", "==", uid));
+    const querySnapshot = await getDocs(q);
+
+    const activities = [];
+
+    querySnapshot.forEach((doc) => {
+      activities.push({ id: doc.id, ...doc.data() });
+    });
+
+    return activities;
+  } catch (error) {
+    console.error("Error fetching activities:", error);
+    throw error;
+  }
+};
+
+// Store an acivity generated from ChatGPT API
+const createActivity = async (userId, activityData) => {
+  const newActivityData = { ...activityData };
+
+  try {
+    // Create the activity document in Firestore
+    const docRef = await addDoc(collection(db, "activities"), {
+      userId,
+      ...newActivityData,
+    });
+
+    return docRef.id;
+  } catch (error) {
+    console.error("Error storing activity:", error);
+    throw error;
+  }
+};
+
+// Delete an activity by its ID
+const deleteActivity = async (activityId) => {
+  try {
+    const activityRef = doc(db, "activities", activityId);
+    await deleteDoc(activityRef);
+  } catch (error) {
+    console.error("Error deleting activity:", error);
+    throw error;
+  }
+};
+
+const getActivityById = async (activityId) => {
+  try {
+    const docRef = doc(db, "activities", activityId);
+    const docSnap = await getDoc(docRef);
+
+    if (!docSnap.exists) {
+      // document does not exist
+      return null;
+    } else {
+      return { id: docSnap.id, ...docSnap.data() };
+    }
+  } catch (error) {
+    console.error("Error fetching activity:", error);
+    throw error;
+  }
+};
+
+export { createActivity, deleteActivity, getActivitiesByUid, getActivityById };
